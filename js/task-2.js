da5c5e2b065a6d981ebf97bafd98190338c48864
Natali-Gal <suhovale@gmail.com>
2020-12-09 23:45:13 +0200

    task-1 - task-7

--- js/task-2.js
+++ js/task-2.js
@@ -1,34 +1,34 @@
-// const calculateEngravingPrice = function(message, pricePerWord) {
-//     const words = message.split(' ');
-//     console.log(words);
-//     const count = words.length;
-//     console.log(count);
-//     console.log('Цена граировки одного слова: ', pricePerWord);
+const calculateEngravingPrice = function(message, pricePerWord) {
+    const words = message.split(' ');
+    console.log(words);
+    const count = words.length;
+    console.log(count);
+    console.log('Цена граировки одного слова: ', pricePerWord);
 
-//     const priceAll = count * pricePerWord;
-//     const total = ` Цена гравировки всех слов в строке: ${priceAll}`
-//     return (total);
+    const priceAll = count * pricePerWord;
+    const total = ` Цена гравировки всех слов в строке: ${priceAll}`
+    return (total);
     
-// };
+};
 
-// console.log(
-//     calculateEngravingPrice(
-//       'Proin sociis natoque et magnis parturient montes mus',
-//       10,
-//     ),
-// ); // 80
+console.log(
+    calculateEngravingPrice(
+      'Proin sociis natoque et magnis parturient montes mus',
+      10,
+    ),
+); // 80
   
-// console.log(
-//     calculateEngravingPrice(
-//       'Proin sociis natoque et magnis parturient montes mus',
-//       20,
-//     ),
-// ); // 160
+console.log(
+    calculateEngravingPrice(
+      'Proin sociis natoque et magnis parturient montes mus',
+      20,
+    ),
+); // 160
   
-// console.log(
-//     calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
-// ); // 240
+console.log(
+    calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
+); // 240
   
-// console.log(
-//     calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
-// ); // 120
+console.log(
+    calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
+); // 120
